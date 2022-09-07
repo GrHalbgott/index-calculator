03dcf7d503378140759e3f29f33ab1a58c293876
GrHalbgott <gr.halbgott53@web.de>
2022-09-07 02:05:21 +0200

    changed the whole script to argparse and added functionalities

---
+++ src/indices.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Functions to calculate the indices"""
+
+import reading
+import glob
+import numpy as np
+
+# The functions all have the same structure:
+# 1. handle resolution input (test if empty -> highest, if not 10 -> change 08 to 8A)
+# 2. look for files with specific bands in their names using glob.glob and *
+# 3. parse the path to the files into read_raster() with clip information
+# 4. calculate index with read rasterfiles, save results as txt-file and return the index as ndarray next to the final resolution after handling
+
+
+def ndvi_calc(resolution, raster_path, clip_shape):
+    """Calculation of the NDVI (Normalized Difference Vegetation Index)"""
+    if resolution == "" or resolution == "10":
+        resolution = "10"
+        for item in glob.glob(
+            raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B08*.jp2"
+        ):
+            b8_path = item
+    elif resolution != "10":
+        for item in glob.glob(
+            raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B8A*.jp2"
+        ):
+            b8_path = item
+    for item in glob.glob(
+        raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B04*.jp2"
+    ):
+        b4_path = item
+    b4 = reading.read_raster(b4_path, clip_shape)
+    b8 = reading.read_raster(b8_path, clip_shape)
+    np.seterr(divide="ignore", invalid="ignore")
+    ndvi = (b8 - b4) / (b8 + b4)
+    np.savetxt("./data/ndvi.txt", ndvi)
+    return ndvi, resolution
+
+
+def ndmi_calc(resolution, raster_path, clip_shape):
+    """Calculation of the NDMI (Normalized Difference Moisture Index)"""
+    if resolution == "":
+        resolution = "20"
+    elif resolution == "10":
+        print("NDMI cannot be calculated with a spatial resolution of 10m.")
+        resolution = "20"
+    for item in glob.glob(
+        raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B8A*.jp2"
+    ):
+        b8a_path = item
+    for item in glob.glob(
+        raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B11*.jp2"
+    ):
+        b11_path = item
+    b8a = reading.read_raster(b8a_path, clip_shape)
+    b11 = reading.read_raster(b11_path, clip_shape)
+    np.seterr(divide="ignore", invalid="ignore")
+    ndmi = (b8a - b11) / (b8a + b11)
+    np.savetxt("./data/ndmi.txt", ndmi)
+    return ndmi, resolution
+
+
+def ndwi_calc(resolution, raster_path, clip_shape):
+    """Calculation of the NDWI (Normalized Difference Water Index)"""
+    if resolution == "" or resolution == "10":
+        resolution = "10"
+        for item in glob.glob(
+            raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B08*.jp2"
+        ):
+            b8_path = item
+    elif resolution != "10":
+        for item in glob.glob(
+            raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B8A*.jp2"
+        ):
+            b8_path = item
+    for item in glob.glob(
+        raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B03*.jp2"
+    ):
+        b3_path = item
+    b3 = reading.read_raster(b3_path, clip_shape)
+    b8 = reading.read_raster(b8_path, clip_shape)
+    np.seterr(divide="ignore", invalid="ignore")
+    ndwi = (b3 - b8) / (b3 + b8)
+    np.savetxt("./data/ndwi.txt", ndwi)
+    return ndwi, resolution
+
+
+def reip_calc(resolution, raster_path, clip_shape):
+    """Calculation of the REIP (Red-Edge Inflection Point)"""
+    if resolution == "":
+        resolution = "20"
+    elif resolution == "10":
+        print("REIP cannot be calculated with a spatial resolution of 10m.")
+        resolution = "20"
+    for item in glob.glob(
+        raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B04*.jp2"
+    ):
+        b4_path = item
+    for item in glob.glob(
+        raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B05*.jp2"
+    ):
+        b5_path = item
+    for item in glob.glob(
+        raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B06*.jp2"
+    ):
+        b6_path = item
+    for item in glob.glob(
+        raster_path + "*/GRANULE/*/IMG_DATA/R" + resolution + "m/*_B07*.jp2"
+    ):
+        b7_path = item
+    b4 = reading.read_raster(b4_path, clip_shape)
+    b5 = reading.read_raster(b5_path, clip_shape)
+    b6 = reading.read_raster(b6_path, clip_shape)
+    b7 = reading.read_raster(b7_path, clip_shape)
+    np.seterr(divide="ignore", invalid="ignore")
+    reip = 705 + 40 * ((b4 + b7) / 2 - b5) / (b6 - b5)
+    np.savetxt("./data/reip.txt", reip)
+    return reip, resolution
--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Calculate indices"""
+"""Calculation of indeces"""
 
 
 import utils
 import sys
-import matplotlib.pyplot as plt
-from rasterio.plot import show as rshow
 import time
 
 
@@ -15,26 +13,26 @@ def main():
     Reads raster files as NumPy-arrays and calculate the desired index
     :return: raster image
     """
+    (
+        index_name,
+        clip_shape,
+        resolution,
+        optional_val,
+        want_plot_saved,
+    ) = utils._check_input_arguments()
 
     starttime = time.time()
 
     raster_path = r"./data/raster/"
-    if len(sys.argv) < 2:
-        print(
-            "Please use the required system arguments. Call the program with:\n $ python src/main.py [shape_input_file_name] [index_name {ndvi, ndmi, ndwi, reip}]"
-        )
-        sys.exit()
-    elif len(sys.argv) < 3:
-        index_name = sys.argv[1].lower()
-    else:
-        index_name = sys.argv[2].lower()
 
     print("Calculating {}...".format(index_name))
     try:
-        result = utils.calc_index(index_name, raster_path)
+        result, calc_resolution = utils.index_calculator(
+            index_name, resolution, raster_path, clip_shape, optional_val
+        )
     except Exception as err:
         print(
-            "...error occured when calculating the {}: {}.\n Please check your input files.".format(
+            "...error occured when calculating the {}: {}.\n Please check your input arguments and files.".format(
                 index_name, str(err)
             )
         )
@@ -44,31 +42,13 @@ def main():
 
     # Print out the information to user
     print(
-        "Finished calculating the {}. \n The script took {:.2f} seconds to run.".format(
-            index_name.upper(), stoptime - starttime
+        "Finished calculating the {} with a spatial resolution of {}m. \n The script took {:.2f} seconds to run.".format(
+            index_name.upper(), calc_resolution, stoptime - starttime
         )
     )
 
-    plt.figure()
-    plt.title("Calculated {} of region of interest".format(index_name.upper()))
-    plt.xlabel("X-Axis")
-    plt.ylabel("Y-Axis")
-    if index_name == "ndvi":
-        plt.imshow(result, cmap="RdYlGn")
-        plt.clim(-0.2, 0.6)
-    elif index_name == "ndmi":
-        plt.imshow(result, cmap="jet_r")
-        plt.clim(-0.2, 0.4)
-    elif index_name == "ndwi":
-        plt.imshow(result, cmap="seismic_r")
-        plt.clim(-0.8, 0.8)
-    else:
-        plt.imshow(result)
-    # Plot a colorbar with the same height as the plot
-    im_ratio = result.shape[0] / result.shape[1]
-    plt.colorbar(fraction=0.04625 * im_ratio)
-    plt.tight_layout()
-    plt.show()
+    # Plot the result (ndarray)
+    utils.plot_result(index_name, result, calc_resolution, want_plot_saved)
 
 
 if __name__ == "__main__":
